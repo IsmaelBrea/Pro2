07fd2ded33ba20bf32651a950a74f2b6da7539bb
user-name <ismael.brea@udc.es>
2024-02-20 13:56:41 +0100

    p1 commit nombres y apellidos

--- P1/Plantilla-Template_P1/P1/dynamic_list.c
+++ P1/Plantilla-Template_P1/P1/dynamic_list.c
@@ -1,12 +1,10 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
+ * AUTHOR 1: ISMAEL BREA ARIAS    LOGIN 1: ismael.brea
+ * AUTHOR 2: DIEGO ROMÁN POSE     LOGIN 2: diego.roman
  * DATE: ** / ** / **
  */
-
 #include "dynamic_list.h"
 
 /* Write your code here... */
--- P1/Plantilla-Template_P1/P1/dynamic_list.h
+++ P1/Plantilla-Template_P1/P1/dynamic_list.h
@@ -1,9 +1,8 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
+ * AUTHOR 1: ISMAEL BREA ARIAS    LOGIN 1: ismael.brea
+ * AUTHOR 2: DIEGO ROMÁN POSE     LOGIN 2: diego.roman
  * DATE: ** / ** / **
  */
 
