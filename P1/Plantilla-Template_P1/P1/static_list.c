77ab46f58f1b035398acaa0aa39b5570c737d0d2
user-name <ismael.brea@udc.es>
2024-02-21 12:32:01 +0100

    static list .c

--- P1/Plantilla-Template_P1/P1/static_list.c
+++ P1/Plantilla-Template_P1/P1/static_list.c
@@ -8,4 +8,39 @@
 
 #include "static_list.h"
 
-/* Write your code here... */
+/*  Write your code here */
+#include <string.h>
+
+void createEmptyList(tList *list){  //la lista se pasa por referencia
+    list->lastPos = LNULL;
+}
+
+bool isEmptyList(tList list){    //la lista se pasa por valor
+    return list.lastPos == LNULL;
+}
+
+tPosL first(tList list) {        //la lista se pasa por valor
+    return 0;
+}
+
+tPosL last(tList list){           //la lista se pasa por valor
+    return list.lastPos;
+}
+
+tPosL next(tPosL pos, tList list){  //la lista se pasa por valor
+    if(pos == list.lastPos){
+       return LNULL;
+    }else{
+        return ++pos;
+    }
+}
+
+tPosL previous(tPosL pos, tList list){  //la lista se pasa por valor
+    return --pos; /* no es necesario especificar que ocurre cuando p es el primer elemento de la lista,0, porque en este
+                      *caso p-- sería igual a -1 que es nuestro LNULL */
+}
+
+void insertItem(tItemL item, tPosL pos, tList *list){  //la lista se pasa por referencia
+
+
+}
