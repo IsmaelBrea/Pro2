830630e1ff4aea52e5e7d24e29eb554cf4d6b20b
user-name <ismael.brea@udc.es>
2024-02-26 21:46:44 +0100

    static list .h final*

--- P1/Plantilla-Template_P1/P1/static_list.c
+++ P1/Plantilla-Template_P1/P1/static_list.c
@@ -40,7 +40,54 @@ tPosL previous(tPosL pos, tList list){  //la lista se pasa por valor
                       *caso p-- sería igual a -1 que es nuestro LNULL */
 }
 
-void insertItem(tItemL item, tPosL pos, tList *list){  //la lista se pasa por referencia
+bool insertItem(tItemL item, tPosL pos, tList *list){  //la lista se pasa por referencia
+    tPosL i;
+
+    if(list->lastPos == MAX-1){
+        return false;
+    }else{
+        list->lastPos++;
+        if(pos  == LNULL){
+            list->data[list->lastPos] = item;
+        }else{
+            for(i=list ->lastPos;i>=pos+1;i--)
+                list->data[i]=list->data[i-1];
+            list->data[pos]= item;
+        }
+        return true;
+    }
+}
+
+void deleteAtPosition (tPosL pos,tList *list){     //la lista se pasa por referencia
+    tPosL i;
+
+    list->lastPos--;       //actualizamos lastPos al haber un elemento menos en la lista
+    for(i=pos;i<=list->lastPos;i++){   //recorremos la lista desde p hasta el final
+        list->data[i] = list->data[i+1];  //cambiando el elemento actual por el siguiente eliminando p
+    }
+}
 
 
+tItemL getItem(tPosL pos, tList list){//la lista se pasa por valor
+    return list.data[pos];
+    }
+
+
+void updateItem (tItemL item, tPosL pos, tList *list){ //la lista se pasa por referencia
+    list->data[pos]=item;
+}
+
+
+tPosL findItem (tUserName name, tList list){     //la lista se pasa por valor
+    tPosL pos;
+    if(list.lastPos==LNULL){
+        return LNULL;
+    }else{
+        for(pos=0;(pos<list.lastPos )&&(strcmp(list.data[pos].userName,name)!=0);pos++);
+        if(strcmp(list.data[pos].userName,name)==0){
+            return pos;
+        }else{
+            return LNULL;
+        }
+    }
 }
